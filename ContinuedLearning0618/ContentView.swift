e66f4de2893c319681ecd8d8104a8e07666d4b2f
Gregory Li <gregoryli2020@icloud.com>
2022-06-18 00:34:34 +0800

    Initial Commit

---
+++ ContinuedLearning0618/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ContinuedLearning0618
+//
+//  Created by Gregory Li on 2022/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ContinuedLearning0618/ContinuedLearning0618App.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearning0618App.swift
+//  ContinuedLearning0618
+//
+//  Created by Gregory Li on 2022/6/18.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearning0618App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
